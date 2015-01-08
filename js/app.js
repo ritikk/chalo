f3c018614a0311ab6e364d9ad96bedb0335fe474
Ritik Khatwani <rkhatwani@homeaway.com>
2015-01-08 12:50:40 +0530

    Moved angular code to app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['chalo'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,9 +15,63 @@ angular.module('starter', ['ionic'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+    .state('page3', {
+      url: '/welcome',
+      templateUrl: 'page3.html'
+    })
+    
+    .state('page5', {
+      url: '/signup/name',
+      templateUrl: 'page5.html'
+    })
+    
+    .state('page6', {
+      url: '/signup/email',
+      templateUrl: 'page6.html'
+    })
+    
+    .state('page7', {
+      url: '/signup/phone',
+      templateUrl: 'page7.html'
+    })
+    
+    .state('page8', {
+      url: '/signup/contacts',
+      templateUrl: 'page8.html'
+    })
+    
+    .state('page9', {
+      url: '/chalo',
+      templateUrl: 'page9.html'
+    })
+    
+    .state('page10', {
+      url: '/chalo/advanced',
+      templateUrl: 'page10.html'
+    })
+    
+    .state('page11', {
+      url: '/chalo/contacts',
+      templateUrl: 'page11.html'
+    })
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  
+  $urlRouterProvider.otherwise('/welcome');
+  
 });
